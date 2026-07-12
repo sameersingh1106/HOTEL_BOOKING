546a525d3f9af1adb5ebd922ed11be98d244e0c7
Sameer Singh <sameersingh@Sameers-MacBook-Air.local>
2026-07-12 16:29:26 +0530

    Initial commit

--- Backend/config/nodemailer.js
+++ Backend/config/nodemailer.js
@@ -2,11 +2,11 @@ import nodemailer from "nodemailer";
 
 const nodemailer = require("nodemailer");
 
-// Create a transporter using SMTP
+
 const transporter = nodemailer.createTransport({
   host: "smtp-relay.brevo.com",
   port: 587,
-  secure: false, // use STARTTLS (upgrade connection to TLS after connecting)
+  secure: false, 
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
